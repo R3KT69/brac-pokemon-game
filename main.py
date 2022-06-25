70d26df14fe78babe9797f1c47e8e5d681da6b64
_r3kt69 <74340727+R3KT69@users.noreply.github.com>
2022-06-25 09:59:08 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+# Making Character object for pokemon characters
 class Character:
-    name = ""
-    hp = 50
-    attack_pow = 20
-    type = ["fire", "water", "grass"]
+    name = "" # Their name
+    hp = 50 # Their initial health
+    attack_pow = 20 # Their initial attack power
+    type = ["fire", "water", "grass"] # Array of available attack types
 
 # Fire Type
 Charmander = Character()
@@ -25,6 +26,8 @@ Bulbasaur.name = Character.name = "Bulbasaur"
 Chikorita = Character()
 Chikorita.type = Character.type[2]
 Chikorita.name = Character.name = "Chikorita"
+
+# Character.type[0] means fire, Character.type[1] means water, Character.type[2] means grass.
 
 ash_type = ""
 gary_type = ""
@@ -77,7 +80,7 @@ elif gary_input == "Chikorita":
     choose_character_gary = Chikorita
 
 # 0 = fire, 1 = water, 2 = grass
-if ash_type == Character.type[0] and gary_type == Character.type[0]:
+if ash_type == Character.type[0] and gary_type == Character.type[0]: # making equal affinity for same types. (fire vs fire, water vs water, grass vs grass)
     type_affinity1 = 1
     type_affinity2 = 1
 elif ash_type == Character.type[1] and gary_type == Character.type[1]:
@@ -106,13 +109,17 @@ elif ash_type == Character.type[2] and gary_type == Character.type[0]: # grass w
     type_affinity1 = 0.5
     type_affinity2 = 2
 
-turn = 0
-remaining_hp1 = choose_character_ash.hp
-remaining_hp2 = choose_character_gary.hp
+turn = 0 # Game turn
+remaining_hp1 = choose_character_ash.hp # Health of the character choosen by Ash
+remaining_hp2 = choose_character_gary.hp # Health of the character choosen by Gary
 
+# Main game loop
 while True:
+    # Incrementing turns
     turn += 1
     print("===", "Turn", turn,"===")
+    
+    # Remaining hp = previous hp – attack power * type affinity
     remaining_hp1 = remaining_hp1 - (choose_character_gary.attack_pow * type_affinity2)
     remaining_hp2 = remaining_hp2 - (choose_character_ash.attack_pow * type_affinity1)
     if remaining_hp1 < 0:
@@ -121,6 +128,8 @@ while True:
         remaining_hp2 = 0
     print(choose_character_ash.name, "has", int(remaining_hp1), "hp left")
     print(choose_character_gary.name, "has", int(remaining_hp2), "hp left")
+    
+    # Loop break logic
     if remaining_hp1 <= 0 or remaining_hp2 <= 0:
         if remaining_hp1 > remaining_hp2:
             print(choose_character_ash.name, "and Ash Wins!")
@@ -129,3 +138,5 @@ while True:
         else:
             print("It's a tie!")
         break
+
+# The end!
